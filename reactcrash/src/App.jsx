a7cac504b8b34aae0be829a1b7874e6a21f29ad8
Natejpg <nathan11.abreu@gmail.com>
2024-01-11 01:22:43 -0300

    Studying CRUD

--- reactcrash/src/App.jsx
+++ reactcrash/src/App.jsx
@@ -1,13 +1,16 @@
 import { Home } from "./components/Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import About from "./components/About";
+import Crud from "./components/Crud";
 
 function App() {
   return (
     <BrowserRouter>
-    <Routes>
-         <Route path = "Home" index element = {<Home/>}/>
-         <Route path="about" element = {<About/>}/>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="home" element={<Home />} />
+        <Route path="CRUD" element={<Crud />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ reactcrash/src/components/Crud.jsx
@@ -0,0 +1,80 @@
+import React from "react";
+import { useState } from "react";
+
+const Crud = () => {
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState("");
+  const [editingTask, setEditingTask] = useState(null);
+  const [editedTaskText, setEditedTaskText] = useState("");
+
+  const AddTask = () => {
+    if (newTask.trim() != "") {
+      setTasks([...tasks, { id: Date.now(), text: newTask }]);
+      setNewTask("");
+    }
+  };
+
+  const DeleteTask = (taskID) => {
+    const updatedTasks = tasks.filter((task) => task.id !== taskID);
+    setTasks(updatedTasks);
+    if (editingTask == taskID) {
+      setEditingTask(null);
+      setEditedTaskText("");
+    }
+  };
+
+  const EditTask = (taskID, taskText) => {
+    setEditingTask(taskID);
+    setEditedTaskText(taskText);
+  };
+
+  const SaveEditTask = () => {
+    const updatedTasks = tasks.map((task) =>
+      task.id === editingTask ? { ...task, text: editedTaskText } : task
+    );
+
+    setTasks(updatedTasks);
+    setEditingTask(null);
+    setEditedTaskText("");
+  };
+
+  return (
+    <div>
+      <h1>CRUD</h1>
+      <input
+        type="text"
+        placeholder="Enter your task"
+        value={newTask}
+        onChange={(e) => setNewTask(e.target.value)}
+      />
+      <button onClick={AddTask}>Add</button>
+      <ul>
+        {tasks.map((task) => (
+          <li key={task.id}>
+            {editingTask === task.id ? (
+              <div>
+                <input
+                  type="text"
+                  placeholder="edit your text"
+                  value={editedTaskText}
+                  onChange={(e) => setEditedTaskText(e.target.value)}
+                />
+                <button onClick={SaveEditTask}>Save</button>
+              </div>
+            ) : (
+              <div>
+                {task.text}
+                <button onClick={() => EditTask(task.id, task.text)}>
+                  Edit
+                </button>
+                <button onClick={() => DeleteTask(task.id)}>Delete</button>
+              </div>
+            )}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Crud;
