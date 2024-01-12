686c2f8da0551de32484d7738aab74d085d0fbee
Natejpg <nathan11.abreu@gmail.com>
2024-01-12 19:41:12 -0300

    Designing the front-end

---
+++ crudreactfirebasepoc/src/components/Crud.js
@@ -0,0 +1,53 @@
+import React, {useState, useEffect} from 'react'
+
+const Crud = () => {
+
+    const [quotes, setQuotes] = useState([]);
+
+
+    useEffect(() => {
+
+        const FetchData = async () => {
+
+            try {
+
+                 const response = await fetch("https://quotesapi-c04u.onrender.com/");         
+                  const data = await response.json();
+
+                  const singleQuoteIndex = Math.floor(Math.random() * data.length)
+                  const singleQuote = data[singleQuoteIndex];
+
+                  setQuotes(singleQuote);
+
+            } catch(error){
+
+                console.log("Couldnt load the API", error);
+
+            }
+        }
+
+        FetchData();
+
+    },[])
+
+  return (
+    <div className='component'>
+        <div className='inputContainer'>
+            <h1>Crud</h1>
+            <div className='inputBx'>
+               <input type='text' placeholder='enter your text'/>
+               <button>New Task</button>
+            </div>
+        </div>
+        <div className='tasksContainer'>TEST</div>
+            <div className='quotesContainer'>
+                <div className='quotesBx'>
+                    {quotes && <h1>{quotes.text}</h1>}
+                    {quotes && <p>{quotes.author}</p>}
+                 </div>
+            </div>
+    </div>
+  )
+}
+
+export default Crud
