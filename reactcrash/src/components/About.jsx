26103c2c534ab1bc12020f44381b5804231a5a71
Natejpg <nathan11.abreu@gmail.com>
2024-01-12 02:02:36 -0300

    Finishing crash course

--- reactcrash/src/components/About.jsx
+++ reactcrash/src/components/About.jsx
@@ -1,17 +1,41 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 const About = () => {
+  const [randomQuote, setRandomQuote] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("https://quotesapi-c04u.onrender.com/");
+        const data = await response.json();
+
+        const randomIndex = Math.floor(Math.random() * data.length);
+        const randomPhrase = data[randomIndex];
+
+        setRandomQuote(randomPhrase);
+      } catch (error) {
+        console.error("Data couldnt be found", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div>
       <h1>
         Congratz, you've managed to learn about Ternary, Flags, Hooks, State,
-        Props and Routes.
+        Effect, Props and Routes.
       </h1>
       <Link to={"/Home"}>Go back to the Home Page</Link>
       <Link to={"/Crud"}>
         <button>Go to the CRUD</button>
       </Link>
+      <h1>Phrase of the day</h1>
+      {randomQuote && <h3>{randomQuote.text}</h3>}
+      {randomQuote && <p>{randomQuote.author}</p>}
     </div>
   );
 };
