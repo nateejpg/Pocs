3a229cc76b74c033cc5efd6923f704c93ea46903
Natejpg <nathan11.abreu@gmail.com>
2024-02-10 14:32:14 -0300

    Finishing POC
    -Added the home component on the front.

--- pocreactmysqlnode/client-side/src/Pages/Home.jsx
+++ pocreactmysqlnode/client-side/src/Pages/Home.jsx
@@ -1,10 +1,46 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import {Link} from "react-router-dom"
+import axios from "axios"
 
 const Home = () => {
+
+  const [books, setBooks] = useState([]);
+
+useEffect(() => {
+
+  const fetchBooks = async () => {
+
+    try{
+    const res = await axios.get("http://localhost:8800/home");
+    setBooks(res.data)
+    console.log("data successfully fetched",res);
+
+    }catch(err){
+      console.log(err);
+    }
+    
+  }
+
+  fetchBooks();
+
+},[])
+
+
   return (
     <div>
         <h1>Home part where will be displayed show recommendations, movies in the theater and so forth</h1>
+        <div className='books'>
+        {books.map((book) => (
+          <div key={book.id} className='book'>
+            <li>
+            {book.title}
+            {book.description}
+            {book.rating}
+            {book.cover}
+            </li>
+            </div>
+        ))}
+        </div>
         <Link to={"/login"}><button>Login</button></Link>
     </div>
 
